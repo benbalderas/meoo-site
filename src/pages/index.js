ea67f3daeb442b925947be837db11bb5ad7d08f8
Benjamin Balderas <ben@Paris-Libere.local>
2019-04-26 15:56:20 -0500

    Watch video text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Changing the way people interact with museums.</h1>
+    <p>Meoo is an app that empowers everyone visiting a museum—enhancing their experience with tools they already have:</p>
+    <p>Their mobile phone.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch Video</Link>
   </Layout>
 )
 
